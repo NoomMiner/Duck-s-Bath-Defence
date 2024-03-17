96bb25e0b0dcdd563dbee3090479d96b48a691b8
dja322 <127354122+dja322@users.noreply.github.com>
2024-03-16 19:57:11 -0700

    Updates website to current build
    Uploads the current build onto the website for anyone to view, as well as editing the website itself to look better and function differently.

---
+++ Builds/ducksFunction.js
@@ -0,0 +1,14 @@
+function gotoGame() 
+{
+      window.location.href = "gameWebPage.html";
+}
+
+function gotoHome() 
+{
+      window.location.href = "./index.html";
+}
+
+function gotoLeaderboard() 
+{
+      window.location.href = "leaderboard.php";
+}

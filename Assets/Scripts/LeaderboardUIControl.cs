cee26aea971d69e7a2ad015fd550e1d52bbaf614
pehilbert <pehilbert03@gmail.com>
2024-03-23 10:42:55 -0700

    Refactored leaderboard entry creator, and added validations for each input

--- Assets/Scripts/LeaderboardUIControl.cs
+++ Assets/Scripts/LeaderboardUIControl.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
@@ -12,12 +13,17 @@ public class LeaderboardUIControl : MonoBehaviour
     {
      // public fields
      public GameObject nameInputObject;
-     public GameObject scoreInputObject;
+     public GameObject scoreText;
      public GameObject messageText;
-     public bool debug;
+     public string playerName;
+     public int score;
+     public int wave;
+     public string mode;
+     public bool showMessage;
      
      // private fields
      private string addScoreURL = "https://ec2-18-117-249-64.us-east-2.compute.amazonaws.com/createlbentry.php?";
+     private float timeoutTime = 5;
 
      // Start is called before the first frame update
      void Start()
@@ -28,7 +34,16 @@ public class LeaderboardUIControl : MonoBehaviour
     // Update is called once per frame
      void Update()
          {
-          // pass
+          TMP_Text scoreTMP;
+
+          if (scoreText.TryGetComponent<TMP_Text>(out scoreTMP))
+            {
+             scoreTMP.SetText("Your score was: " + score.ToString());
+            }
+          else
+            {
+             scoreTMP.SetText("Score cannot be displayed");
+            }
          }
 
      public void submitButton()
@@ -36,7 +51,7 @@ public class LeaderboardUIControl : MonoBehaviour
          string postScoresResult = postScores();
          TMP_Text messageTMP;
 
-         if (debug && messageText.TryGetComponent<TMP_Text>(out messageTMP))
+         if (showMessage && messageText.TryGetComponent<TMP_Text>(out messageTMP))
             {
              messageTMP.SetText(postScoresResult);
              messageTMP.enabled = true;
@@ -54,24 +69,74 @@ public class LeaderboardUIControl : MonoBehaviour
      private string postScores()
         {
          TMP_InputField nameTMP;
-         TMP_InputField scoreTMP;
 
-         if (!nameInputObject.TryGetComponent<TMP_InputField>(out nameTMP) ||
-             !scoreInputObject.TryGetComponent<TMP_InputField>(out scoreTMP))
+         if (!nameInputObject.TryGetComponent<TMP_InputField>(out nameTMP))
             {
-             return "Could not read from text inputs.";
+             return "Could not read from name input.";
+            }
+
+         playerName = nameTMP.text;
+
+         if (!validateName(playerName))
+            {
+             return "Invalid name";
+            }
+
+         if (!validateScore(score))
+            {
+             return "Invalid score";
+            }
+
+         if (!validateWave(wave))
+            {
+             return "Invalid wave";
+            }
+
+         if (!validateMode(mode))
+            {
+             return "Invalid mode";
             }
 
          string postURL = addScoreURL +
-                          "name=" + nameTMP.text +
-                          "&score=" + scoreTMP.text +
-                          "&wave=" + "1" +
-                          "&mode=" + "Classic";
+                          "name=" + UnityWebRequest.EscapeURL(playerName) +
+                          "&score=" + UnityWebRequest.EscapeURL(score.ToString()) +
+                          "&wave=" + UnityWebRequest.EscapeURL(wave.ToString()) +
+                          "&mode=" + UnityWebRequest.EscapeURL(mode);
 
          UnityWebRequest postRequest = UnityWebRequest.Post(postURL, "", "text");
          postRequest.certificateHandler = new GuaranteedCertificate();
          postRequest.SendWebRequest();
 
-         return "Result of request to " + addScoreURL + " was: " + postRequest.result.ToString();
+         float timeSent = Time.time;
+
+         while (Time.time - timeSent < timeoutTime)
+            {
+             if (postRequest.result == UnityWebRequest.Result.Success)
+               {
+                return "Score successfully posted!";
+               }
+            }
+
+         return "Request timed out, current status: " + postRequest.result.ToString();
         }
+
+      private bool validateName(string name)
+         {
+          return name.Length == 3;
+         }
+
+      private bool validateScore(int score)
+         {
+          return score >= 0;
+         }
+
+      private bool validateWave(int wave)
+         {
+          return wave >= 1;
+         }
+
+      private bool validateMode(string mode)
+         {
+          return mode.Length > 0;
+         }
     }

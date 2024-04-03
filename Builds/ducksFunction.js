b2e1174bfa1d71228f9f836fa8af291ccc050f6d
dja322 <127354122+dja322@users.noreply.github.com>
2024-04-02 17:54:58 -0700

    Add files via upload

--- Builds/ducksFunction.js
+++ Builds/ducksFunction.js
@@ -1,14 +1,17 @@
 function gotoGame() 
 {
       window.location.href = "gameWebPage.html";
+      console.log("Successfully went to game");
 }
 
 function gotoHome() 
 {
       window.location.href = "./index.html";
+      console.log("Successfully went to home");
 }
 
 function gotoLeaderboard() 
 {
       window.location.href = "leaderboard.php";
+      console.log("Successfully went to leaderboard");
 }

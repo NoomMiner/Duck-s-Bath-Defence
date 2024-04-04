bb56819299f45a85cf2e89b36a7cf302da003259
pehilbert <pehilbert03@gmail.com>
2024-04-04 15:25:14 -0700

    Set up classes with function stubs

--- Assets/Scripts/AttackType.cs
+++ Assets/Scripts/AttackType.cs
@@ -4,5 +4,44 @@ using UnityEngine;
 
 public interface AttackType
 {
-    
+    public bool attack();
+}
+
+public class SingleClosestTarget : AttackType
+{
+    public bool attack()
+    {
+        return true;
+    }
+
+    private Entity findClosestTarget()
+    {
+        return null;
+    }
+}
+
+public class SingleFurthestTarget : AttackType
+{
+    public bool attack()
+    {
+        return true;
+    }
+
+    private Entity findFurthestTarget()
+    {
+        return null;
+    }
+}
+
+public class AreaOfEffect : AttackType
+{
+    public bool attack()
+    {
+        return true;
+    }
+
+    private List<Entity> getTargetsInRangeOfPosition(Transform tPosition)
+    {
+        return new List<Entity>();
+    }
 }
--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -1,18 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // attributes
+    public String entityName;
+    public AttackType attack;
+    private float currentHealth;
+    public float maxHealth;
+    public float attackCooldown;
+    public float damage;
+    public float range;
+    public TargetFamily family;
+    public TargetFamily targetFamily;
+
+    // private members
+    private float lastAttackTime;
+
+    // runtime functions
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    public void takeDamage(float amount)
+    {
+
+    }
+
+    public void heal(float amount)
+    {
+
+    }
+
+    public void die()
+    {
+
+    }
+}
+
+public enum TargetFamily
+{
+    Tower,
+    Enemy,
+    Drain,
+    None
 }

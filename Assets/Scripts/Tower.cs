93ec95ef0d185b892f53971680ffaeafcf00774a
pehilbert <pehilbert03@gmail.com>
2024-04-23 18:59:07 -0700

    Save

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Tower : Entity
 {
     public GameObject tiles;
-
+    
     public bool placeTower(Vector3 positionToPlace)
     {
         // TODO: place tower on grid
